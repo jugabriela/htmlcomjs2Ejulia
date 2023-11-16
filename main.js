2cd71e4e5a930d45623eb54c908ccdce0490ab75
jugabriela <127451876+jugabriela@users.noreply.github.com>
2023-11-16 20:24:03 -0300

    16.11

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom (idElementoAudio ) {
+    document.querySelector (idElementoAudio).play();
 }
 
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+
+for (let contador = 0; contador <listaDeTeclas.length;contador++)
+{
+const  tecla = listaDeTeclas [contador];
+
+const instrumento = tecla.classList[1];
+
+const idAudio = `#som_${instrumento}`;//template string
+
+tecla.onclick = function () {
+    tocaSomPom(idAudio);
+}
+tecla.onkeydown = function (evento) {
+
+    console.log(evento.cod== 'Space')
+
+if(evento.code === 'Space'){ 
+
+    tecla.classList.add('ativa'); }
+
+if (evento.code === 'Enter') {
+    tecla.classList.add ('ativa');
+}
+
+}
+tecla.onkeyup = fuction () {
+    tecla.classList.remove('ativa');
+
+}
+
+
+
+}
